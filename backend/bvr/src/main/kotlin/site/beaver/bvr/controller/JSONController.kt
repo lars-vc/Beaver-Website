90722e172d26085ef988f1c73a6d65dcf066408e
Lars Van Cauter <larsvancauter@gmail.com>
2021-04-14 11:09:51 +0200

    images woooooork

--- backend/bvr/src/main/kotlin/site/beaver/bvr/controller/JSONController.kt
+++ backend/bvr/src/main/kotlin/site/beaver/bvr/controller/JSONController.kt
@@ -1,6 +1,7 @@
 package site.beaver.bvr.controller
 
 import com.google.gson.Gson
+import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -8,50 +9,72 @@ import org.springframework.web.bind.annotation.RestController
 import site.beaver.bvr.model.AllBeavers
 import site.beaver.bvr.model.Beaver
 import java.io.File
+import org.springframework.util.StreamUtils
+
+import org.springframework.core.io.ClassPathResource
+
+import java.io.IOException
+
+import javax.servlet.http.HttpServletResponse
+
+import org.springframework.web.bind.annotation.RequestMethod
+
 
 @RestController
 @RequestMapping("/api/beaver")
 class JSONController {
+	val preurl = "localhost:8080"
 
 	@GetMapping
-	fun default(): String{
-		return Gson().toJson(
-			hashMapOf<String,String>(
-				"all" to "/api/beaver/all",
-				"random" to "/api/beaver/random"
-			))
+	fun default(): HashMap<String, String> {
+		return hashMapOf(
+			"all" to "$preurl/api/beaver/all",
+			"random" to "$preurl/api/beaver/random"
+		)
 	}
 
 	@GetMapping("/random")
 	fun randomBeaver(): Beaver {
-		return Beaver("/api/beaver/all", "/api/beaver/all/X", "")
+		val dir = JSONController::class.java.getResource("/images")
+		var str = ""
+		File(dir.toURI()).walk().drop(1).shuffled().take(1).forEach {
+			str = it.toString()
+		}
+		val res = str.filter { it.isDigit() }
+		return Beaver("$preurl/api/beaver/all", "$preurl/api/beaver/$res", "$preurl/api/beaver/$res/image")
 	}
 
-	@GetMapping("/all/{beaverId}")
-	fun getBeaver(@PathVariable beaverId: String): Beaver{
-		val img = JSONController::class.java.getResource("/images/beaverpicture ($beaverId).jpg")
-		print(img)
-		return Beaver("/api/beaver/all", "/api/beaver/all/$beaverId", img.toString())
-	}
-
-	@GetMapping("/all/{beaverId}/image")
-	fun getBeaverImage(@PathVariable beaverId: String): String{
-		val img = JSONController::class.java.getResource("/images/beaverpicture ($beaverId).jpg")
-		print(img)
-		return "<html>" +
-				"<body>" +
-				"<img src=$img />" +
-				"</body>" +
-				"</html>"
+	@GetMapping("/{beaverId}")
+	fun getBeaver(@PathVariable beaverId: String): Beaver {
+		return Beaver(
+			"$preurl/api/beaver/all",
+			"$preurl/api/beaver/$beaverId",
+			"$preurl/api/beaver/$beaverId/image"
+		)
 	}
 
 	@GetMapping("/all")
 	fun getAll(): AllBeavers {
 		val res = mutableListOf<String>()
 		val dir = JSONController::class.java.getResource("/images")
-		File(dir.toURI()).walk().forEach{
-			res.add(it.name)
+		File(dir.toURI()).walk().drop(1).forEach {
+
+			res.add(preurl + "/api/beaver/" + it.name.filter { c -> c.isDigit() })
 		}
-		return AllBeavers("/api/beaver", "/api/beaver/all", res)
+		return AllBeavers("$preurl/api/beaver", "$preurl/api/beaver/all", res)
+	}
+
+	@RequestMapping(
+		value = ["/{beaverId}/image"],
+		method = [RequestMethod.GET],
+		produces = [MediaType.IMAGE_JPEG_VALUE]
+	)
+	@Throws(
+		IOException::class
+	)
+	fun getImage(response: HttpServletResponse, @PathVariable beaverId: String) {
+		val imgFile = ClassPathResource("images/beaverpicture ($beaverId).jpg")
+		response.contentType = MediaType.IMAGE_JPEG_VALUE
+		StreamUtils.copy(imgFile.inputStream, response.outputStream)
 	}
 }
